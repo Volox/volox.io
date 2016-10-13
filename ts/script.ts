c2e520ed8ec63a20a8c537b3a0a488e2336989f1
Riccardo Volonterio <voloxxx@gmail.com>
2016-10-13 11:28:32 +0200

    added favicon fixed 3D in FF fixed mouse following behaviour

--- ts/script.ts
+++ ts/script.ts
@@ -1,48 +1,69 @@
-interface NodeListOf<TNode extends Node> extends Array<TNode> {} // trick to work with for-of
+interface NodeListOf<TNode extends Node> extends Array<TNode> { } // trick to work with for-of
 
 const MAX_DEG_X = 50;
 const MAX_DEG_Y = 50;
+let winFollow = false;
 
-const logo = document.querySelector( '.logo' ) as HTMLElement;
-const jobs = document.querySelectorAll( '.job > a' ) as NodeListOf<HTMLElement>;
+const logo = document.querySelector('.logo') as HTMLElement;
+const job = document.querySelectorAll('.job > a') as NodeListOf<HTMLElement>;
 
-function handleMouseMove( e: MouseEvent ) {
+function handleMouseMove(e: MouseEvent) {
   const x = e.pageX;
   const y = e.pageY;
   // console.log( 'x: %d, y: %d', x, y );
 
-  const halfX = window.innerWidth/2;
-  const halfY = window.innerHeight/2;
+  const halfX = window.innerWidth / 2;
+  const halfY = window.innerHeight / 2;
 
   const signX = x > halfX ? 1 : -1;
   const signY = y > halfY ? -1 : 1;
 
-  const xpart = Math.abs( x - halfX );
-  const ypart = Math.abs( y - halfY );
-  //halfX:MAX_DEG_X = x:halfX
-  const degX = (xpart*MAX_DEG_X)/halfX;
-  const degY = (ypart*MAX_DEG_Y)/halfY;
+  const xpart = Math.abs(x - halfX);
+  const ypart = Math.abs(y - halfY);
 
-  // logo.style.transform = `rotateY(${degX*signX}deg)`;
-  // logo.style.transform = `rotateY(${degY*signY}deg)`;
-  logo.style.transform = `rotateY(${degX*signX}deg) rotateX(${degY*signY}deg)`;
+  const degX = (xpart * MAX_DEG_X) / halfX;
+  const degY = (ypart * MAX_DEG_Y) / halfY;
+
+  logo.style.transform = `rotateY(${degX * signX}deg) rotateX(${degY * signY}deg)`;
 }
 
-function toggleFollow( e: MouseEvent ) {
-  const isActive = logo.classList.contains( 'active' );
-  if( isActive ) {
-    window.removeEventListener( 'mousemove', handleMouseMove );
-    logo.style.transform = '';
-    logo.classList.remove( 'active' );
+function isActive() {
+  return logo.classList.contains('active');
+}
+function startFollow(e: MouseEvent) {
+  if (isActive()) return;
+
+  logo.classList.add('active');
+  window.addEventListener('mousemove', handleMouseMove);
+  handleMouseMove(e); // Kick start
+}
+function stopFollow(e: MouseEvent) {
+  if (!isActive()) return;
+
+  window.removeEventListener('mousemove', handleMouseMove);
+  logo.style.transform = '';
+  logo.classList.remove('active');
+}
+
+window.addEventListener('click', e => {
+  if( isActive() ) {
+    winFollow = false;
+    stopFollow( e );
   } else {
-    logo.classList.add( 'active' );
-    window.addEventListener( 'mousemove', handleMouseMove );
-    handleMouseMove( e ); // Kick start
+    winFollow = true;
+    startFollow( e );
   }
-}
+});
 
-window.addEventListener( 'click', toggleFollow );
-for( const element of jobs ) {
-  element.addEventListener( 'mouseenter', toggleFollow );
-  element.addEventListener( 'mouseleave', toggleFollow );
+
+for (const element of job) {
+  const variation = element.dataset['variation' ];
+  element.addEventListener('mouseenter', e => {
+    logo.classList.add(variation);
+    if (!winFollow) startFollow(e);
+  });
+  element.addEventListener('mouseleave', e => {
+    logo.classList.remove(variation);
+    if (!winFollow) stopFollow(e);
+  });
 }
